22d8396124f3b3079b18a553c67d052cd771eff1
Will Gallia <wgallia@gmail.com>
2011-03-14 17:24:51 +0000

    Revert "text wrapping"
    This reverts commit b3a8673883c8157b4c8cf9b87e587d330788da82.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include "ofMain.h"
 #include "ofAppGlutWindow.h"
 
-#include "SPreview.h"
+#include "preview.h"
 
 // - + - + - + - + - + - + - + -
 
@@ -9,6 +9,6 @@ int main() {
 
 	ofAppGlutWindow window;
 	ofSetupOpenGL(&window, 800, 600, OF_WINDOW);
-	ofRunApp(new SPreview());
+	ofRunApp(new preview());
 
 }

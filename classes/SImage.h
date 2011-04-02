fdfd53d7b6be3d98cca42ad7a15abcd69f657b9a
Will Gallia <wgallia@gmail.com>
2011-04-02 15:26:45 +0100

    after a big re-organisation

---
+++ classes/SImage.h
@@ -0,0 +1,26 @@
+/*
+ *  SImage.h
+ *  Sybil
+ *
+ *  Created by Will Gallia on 21/03/2011.
+ *  Copyright 2011 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#ifndef _SIMAGE
+#define _SIMAGE
+
+#include "ofMain.h"
+#include "SItem.h"
+
+class SImage : public SItem {
+	
+private:
+	
+public:
+	
+	
+};
+
+
+#endif
